1f45500eb5bc7b7e4b98841d184944f2b11bdd94
karthikchandu <57186207+karthikchandu@users.noreply.github.com>
2020-03-21 15:36:55 +0530

    Add files via upload

---
+++ f1.php
@@ -0,0 +1,39 @@
+<html>
+<head>
+     <link rel="stylesheet" type="text/css" href="font.css">
+	<title>payment slip</title>
+
+</head>
+<style>
+
+	body{
+			background-image: url("pay1.png");
+			background-size: 100%;
+		}
+	</style>
+
+<body >
+	<center><h1><U>ZOO  AUTHORITY</U></h1></center><br><br><br><br>
+	<center><h1><U>PAYMENT  RECEIPT</U></h1></center>
+    <div class="container">
+    	<table align="center" border="lpx" style="width:600px; line-height:40px;">
+
+			
+	
+		<t>
+			
+			<th>Payment succsessfull</th>
+			
+		</t>
+		
+
+		<th><button onclick="window.location.href='safari.php'">Enjoy the safari visit</button></th>
+        </table><br><br><br>
+
+        <center><h1><U>SAFARI ENTRY TICKET</U></h1></center><br>
+        <center><p><h1><U>DEPARTMENT OF WILDLIFE CONSERVATION</U></h1></p></center>
+   <center>     <p><h2>To explore the wild jungle visit the safari....</h2></p></center><br><br><br><br><marquee bgcolor="green"; direction="left"><h2>Save forest,&nbsp Save animals,&nbsp    Have a Safe    and Happy visit. &nbsp Thank you.......</h2></marquee>
+
+
+</body>
+</html>
